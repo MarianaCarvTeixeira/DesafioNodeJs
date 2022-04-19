3c48a8fa40f39bfdebfa7788686b4174ab89ff31
MarianaCarvTeixeira <mariana.silva.teixeira@hotmail.com>
2022-04-18 21:22:17 -0300

    WIP Employes api

--- app/controllers/employes.js
+++ app/controllers/employes.js
@@ -1,8 +1,82 @@
 const Employes = require("../models/employes");
+
 class EmployesController{
 
-
     async store(req, res) {
+
+    const { name, cpf, office, birthday, situation } = req.body;
+
+
+    if(situation === true){
+        situation = activate
+    } else if(situation === false){
+        situation = deactivate
+    }
+    
+      const employee = {
+          name,
+          cpf,
+          office,
+          birthday,
+          situation
+      }
+  
+      try{
+          await Employes.create(employee)
+  
+          res.status(201).json({
+            employee_id: id,
+            name: employee.name,
+            cpf: employee.cpf,
+            office: employee.office,
+            birthday: employee.birthday,
+            situation: true,
+          })
+      }
+      catch(error){
+          res.status(400).json({error: error})
+      }
+  };
+  async getEmployes(req, res) {
+  
+      try{
+        const employee = await Employes.find()
+  
+          res.status(201).json(employee)
+      }
+      catch(error){
+          res.status(400).json({error: error})
+      }
+  };
+  async updateEmployes(req, res) {
+
+    const { name, office, situation } = req.body;
+  
+      const employee = {
+          name,
+          cpf,
+          office,
+          birthday,
+          situation
+      }
+  
+      try{
+          await Employes.create(employee)
+  
+          res.status(201).json({
+            employee_id: id,
+            name: employee.name,
+            cpf: employee.cpf,
+            office: employee.office,
+            birthday: employee.birthday,
+            situation: employee.situation,
+          })
+      }
+      catch(error){
+          res.status(400).json({error: error})
+      }
+  };
+  async deleteEmployes(req, res) {
 
     const { name, cpf, office, birthday, situation } = req.body;
   
@@ -23,18 +97,6 @@ class EmployesController{
           res.status(400).json({error: error})
       }
   };
-  async getEmployes(req, res) {
-  
-      try{
-        const employee = await Employes.find()
-  
-          res.status(201).json(employee)
-      }
-      catch(error){
-          res.status(400).json({error: error})
-      }
-  };
-  
 }
 
 module.exports = EmployesController
--- app/models/employes.js
+++ app/models/employes.js
@@ -1,12 +1,30 @@
 const mongoose = require("mongoose");
 
+const office = Object.freeze({
+  Gerente: "gerente",
+  Vendedor: "vendedor",
+  Caixa: "caixa",
+});
+
 const Employes = mongoose.model("Employes", {
-  id: Number,
-  name: String,
-  cpf: String,
-  office: String,
-  birthday: Date,
-  situation: Boolean,
+  name: {
+    type: String,
+    required: true,
+  },
+  cpf: {
+    type: String,
+    required: true,
+  },
+  office: {
+    type: String,
+    enum: Object.values(office),
+    required: true,
+  },
+  birthday: {
+    type: Date,
+    required: true,
+  },
+  situation: String,
 });
 
 module.exports = Employes;
