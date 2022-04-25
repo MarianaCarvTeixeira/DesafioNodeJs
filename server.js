6e4d0d4636672e6f542e32e3a4bda5dafaec84b4
MarianaCarvTeixeira <mariana.silva.teixeira@hotmail.com>
2022-04-25 12:05:16 -0300

    Config Mongo

--- server.js
+++ server.js
@@ -1,17 +1,24 @@
-const express= require("express");
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
-const path = require('path');
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const path = require("path");
+const mongoose = require("mongoose");
 
-const routes = require('./src/routes');
+const routes = require("./src/routes");
 
 const app = express();
+
+mongoose.connect("mongodb://localhost:27017/MariaCompany", function (err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("mongo conectado");
+  }
+});
 
 app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3000, function(){
-    console.log("server iniciado")
-});
+app.listen(3000);
