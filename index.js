d278c5b8b8e9a7d274fa5ad36634eb6b88a5ccb7
MarianaCarvTeixeira <mariana.silva.teixeira@hotmail.com>
2022-04-25 11:07:43 -0300

    Recomeçanco

--- index.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const server = require('./server')
-
-mongoose
-  .connect(
-    "mongodb+srv://Admin:Admin@mariacluster.mturj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-  )
-  .then(() => console.log("Connect"), server.listen(3000))
-  .catch((err) => console.log("erro"));
