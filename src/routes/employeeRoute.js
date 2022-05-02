0e5cb9f8b97ee1d50d8dbe8c27cf0bc072e9afbe
MarianaCarvTeixeira <mariana.silva.teixeira@hotmail.com>
2022-05-02 10:22:24 -0300

    UUID

--- src/routes/employeeRoute.js
+++ src/routes/employeeRoute.js
@@ -6,6 +6,6 @@ const router = Router();
 router.post("/", EmployesController.store);
 router.get("/", EmployesController.index);
 router.put("/:_id",EmployesController.update);
-router.delete("/:Employes_id", EmployesController.delete);
+router.delete("/:_id", EmployesController.delete);
 
 module.exports = router;
